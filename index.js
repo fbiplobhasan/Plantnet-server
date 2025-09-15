678ec713e0d8eb4d13ab120c94e2a6ca710d9932
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-09-15 19:37:12 +0600

    add admin,seller,customar route and components

--- index.js
+++ index.js
@@ -102,6 +102,13 @@ async function run() {
       res.send(result);
     })
 
+    // get user role
+    app.get('/users/role/:email', async (req, res) => {
+      const email = req.params.email;
+      const result = await userCollection.findOne({ email });
+      res.send({ role: result?.role })
+    })
+
     // manage users status
     app.patch('/users/:email', async (req, res) => {
       const email = req.params.email;
